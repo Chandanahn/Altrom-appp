52d7aeaae475b94d3cd5c444974c55bc4208666f
Chandana Chandana <chandanc@dhcp-10-120-174-239.wifi.oracle.com>
2024-07-12 10:32:34 +0530

    add

---
+++ src/main/java/com/training/oracle/bms/AlstromApppApplication.java
@@ -0,0 +1,15 @@
+package com.training.oracle.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstromApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstromApppApplication.class, args);
+		
+		System.out.println("Fuckkk offf");
+	}
+
+}
