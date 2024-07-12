3037e1323de780febc65e0d534d0f1934bdc99df
Chandana Chandana <chandanc@dhcp-10-120-174-239.wifi.oracle.com>
2024-07-12 12:35:59 +0530

    assertEquals

--- src/test/java/com/training/oracle/bms/AlstromApppApplicationTests.java
+++ src/test/java/com/training/oracle/bms/AlstromApppApplicationTests.java
@@ -1,5 +1,8 @@
 package com.training.oracle.bms;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,7 @@ class AlstromApppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(190,200);
 	}
 
 }
